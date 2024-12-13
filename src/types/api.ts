8e13620c68e4316a3a3d955b40a482ce82960bcc
Kerem Mert Ada <keremmertada6@gmail.com>
2024-12-13 09:45:45 +0300

    Changes

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,2 +1,2 @@
-export const API_KEY = '70a8843';
-export const API_URL = 'http://www.omdbapi.com/';
+export const API_KEY = process.env.REACT_APP_API_KEY || '';
+export const API_URL = process.env.REACT_APP_API_URL || '';
